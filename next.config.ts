aa1e130565fd2b58016556399586a7987255f793
Puneeth-24 <puneethjains2005@gmail.com>
2025-12-16 20:24:11 +0530

    changed next js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['https://portfolio-theta-ivory-33.vercel.app/'],
+  }
 };
 
 export default nextConfig;
