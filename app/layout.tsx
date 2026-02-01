8807d63bed8947bfb4263a6be68b430673d74145
Puneeth-24 <puneethjains2005@gmail.com>
2026-02-01 17:07:01 +0530

    removed the google tag manager changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Puneeth Jain | Portfolio",
@@ -14,34 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en"><head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NFTK3C42');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager */}
       </head>
       <body className="bg-white text-gray-900 antialiased">
-{/* Google Tag Manager (noscript) */}
-  <noscript>
-    <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-NFTK3C42"
-      height="0"
-      width="0"
-      style={{ display: "none", visibility: "hidden" }}
-    />
-  </noscript>
-  {/* End Google Tag Manager (noscript) */}
-        {children}
+       {children}
       </body>
     </html>
   );
